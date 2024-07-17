85306ae56a4e96537fc54c09611dce88f56f461d
toni-akintola <oakintol@nd.edu>
2024-07-17 15:03:56 -0400

    update to make compatible with modelpy api

--- model.py
+++ model.py
@@ -7,55 +7,64 @@ class HuangTrackRecord:
     def __init__(self):
         # Define Parameters
         self.num_nodes = 30
-        self.graph_type = 'complete'  # 'complete', 'wheel', or 'cycle'
+        self.graph_type = "complete"  # 'complete', 'wheel', or 'cycle'
         self.true_bias = 0.6
-        self.num_steps = 100
 
         # Graph initialization
         self.graph: nx.Graph = None
 
-    def update_credences(self, node_data, coin_flip):
+    def _update_credences(self, node_data, coin_flip):
         ideal_update = self._bayesian_update(node_data, coin_flip)
-        noisy_update = np.random.normal(ideal_update, node_data['b'])
-        node_data['credences'] = node_data['c'] * noisy_update + \
-            (1 - node_data['c']) * node_data['credences']
-        node_data['credences'] = node_data['credences'] / \
-            np.sum(node_data['credences'])
+        noisy_update = np.random.normal(ideal_update, node_data["b"])
+        node_data["credences"] = list(
+            node_data["c"] * noisy_update
+            + (1 - node_data["c"]) * np.array(node_data["credences"])
+        )
+        node_data["credences"] = list(
+            np.array(node_data["credences"]) / np.sum(node_data["credences"])
+        )
 
-    def bayesian_update(self, node_data, coin_flip):
+    def _bayesian_update(self, node_data, coin_flip):
         likelihoods = self._calculate_likelihoods(coin_flip)
-        posteriors = node_data['credences'] * likelihoods
+        posteriors = node_data["credences"] * likelihoods
         return posteriors / np.sum(posteriors)
 
-    def calculate_likelihoods(self, coin_flip):
+    def _calculate_likelihoods(self, coin_flip):
         biases = np.array([0, 0.2, 0.4, 0.6, 0.8, 1.0])
         return biases if coin_flip == 1 else (1 - biases)
 
-    def solicit_testimony(self, node):
+    def _solicit_testimony(self, node):
         neighbors = list(self.graph.neighbors(node))
         node_data = self.graph.nodes[node]
         if neighbors:
-            informants = np.random.choice(neighbors, int(
-                len(neighbors) * node_data['m']), replace=False)
+            informants = np.random.choice(
+                neighbors, int(len(neighbors) * node_data["m"]), replace=False
+            )
             social_update = np.mean(
-                [self.graph.nodes[informant]['credences'] for informant in informants], axis=0)
-            node_data['credences'] = node_data['m'] * social_update + \
-                (1 - node_data['m']) * node_data['credences']
-            node_data['credences'] = node_data['credences'] / \
-                np.sum(node_data['credences'])
+                [self.graph.nodes[informant]["credences"] for informant in informants],
+                axis=0,
+            )
+            node_data["credences"] = list(
+                node_data["m"] * social_update
+                + (1 - node_data["m"]) * np.array(node_data["credences"])
+            )
+            node_data["credences"] = list(
+                np.array(node_data["credences"]) / np.sum(node_data["credences"])
+            )
 
-    def record_track(self, node_data):
+    def _record_track(self, node_data):
         true_bias_vector = np.array(
-            [1 if self.true_bias == bias else 0 for bias in [0, 0.2, 0.4, 0.6, 0.8, 1]])
-        accuracy = 1 - \
-            np.mean((node_data['credences'] - true_bias_vector) ** 2)
-        node_data['track_record'].append(accuracy)
+            [1 if self.true_bias == bias else 0 for bias in [0, 0.2, 0.4, 0.6, 0.8, 1]]
+        )
+        accuracy = 1 - np.mean((node_data["credences"] - true_bias_vector) ** 2)
+        node_data["track_record"].append(accuracy)
+        node_data["curr_brier"] = accuracy
 
     def initialize_graph(self):
         # Initialize graph shape
-        if self.graph_type == 'complete':
+        if self.graph_type == "complete":
             self.graph = nx.complete_graph(self.num_nodes)
-        elif self.graph_type == 'cycle':
+        elif self.graph_type == "cycle":
             self.graph = nx.cycle_graph(self.num_nodes)
         else:
             self.graph = nx.wheel_graph(self.num_nodes)
@@ -63,11 +72,12 @@ class HuangTrackRecord:
         # Initialize agents
         for node in self.graph.nodes():
             initial_data = {
-                'credences': np.ones(6) / 6,
-                'track_record': [],
-                'c': np.random.uniform(0, 1),
-                'b': np.random.uniform(0, 0.2),
-                'm': np.random.uniform(0.05, 0.5)
+                "credences": list(np.ones(6) / 6),
+                "track_record": [],
+                "curr_brier": 1,
+                "c": np.random.uniform(0, 1),
+                "b": np.random.uniform(0, 0.2),
+                "m": np.random.uniform(0.05, 0.5),
             }
             self.graph.nodes[node].update(initial_data)
         return self.graph
--- test.py
+++ test.py
@@ -4,23 +4,10 @@ import networkx as nx
 
 # Get all keys that have numerical values in the graph
 model = HuangTrackRecord()
-initial = model.initialize_graph()
-median_vals = []
-numerical_keys = {key for node, data in initial.nodes(
-    data=True) for key in data if isinstance(data[key], (int, float, complex))}
-print("initial graph data", initial.nodes(data=True))
-print("/n")
-timesteps = 50
-for _ in range(timesteps):
-    updated_graph = model.timestep()
-    timestep_medians = {}
-    print("Credences", _, [data['credences']
-          for node, data in updated_graph.nodes(data=True)])
-    for key in numerical_keys:
-        values = [data[key] for node, data in updated_graph.nodes(
-            data=True) if key in data and data[key] is not None]
-        new_median = statistics.median(values) if values else 0
-        timestep_medians[key] = new_median
-    median_vals.append(timestep_medians)
-print("final graph data", updated_graph.nodes(data=True))
-print("median data", [data for data in median_vals])
+model.initialize_graph()
+
+print("GRAPH_NODES:", model.graph.nodes(data=True, default={}))
+print("GRAPH_EDGES:", model.graph.edges(data=True))
+
+model.timestep()
+print("GRAPH_NODES:", model.graph.nodes(data=True, default={}))
